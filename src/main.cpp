8dccc1c59609b6fa8a7181ab15a107b888aeb64a
ADIOS0022 <tarzan33748@gmail.com.com>
2024-12-06 17:17:51 +0700

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
+#define SWITCH1 15
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(23,OUTPUT);//Red1
+pinMode(19,OUTPUT);//Yellow1
+pinMode(18,OUTPUT);//Green1
+pinMode(5,OUTPUT);//Red2
+pinMode(17,OUTPUT);//Yellow2
+pinMode(16,OUTPUT);//Green2
+pinMode(15,INPUT);//Switch
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(23,LOW);//R1
+  digitalWrite(16,HIGH);//G1
+  digitalWrite(18,LOW);//G2
+  digitalWrite(5,HIGH);//R2
+  delay(1000);
+   digitalWrite(5,LOW);//R2
+  digitalWrite(18,HIGH);//G2
+  digitalWrite(23,HIGH);//R1
+  digitalWrite(16,LOW);//G1
+  delay(1000);
 }
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+  if (digitalRead(SWITCH1) == HIGH)
+  {
+    digitalWrite(23,HIGH);//R1
+    digitalWrite(16,HIGH);//G1
+    delay(400);
+    digitalWrite(18,LOW);//G1
+    digitalWrite(23,LOW);//R1
+    delay(400)
+  }
+  while (digitalRead(SWITCH1) == LOW)
+  {
+    digitalWrite(23,LOW);//R1
+  digitalWrite(16,LOW);//G1
+  digitalWrite(18,HIGH);//G2
+  digitalWrite(5,HIGH);//R2
+  delay(500);
+   digitalWrite(5,LOW);//R2
+  digitalWrite(18,LOW);//G2
+  digitalWrite(23,HIGH);//R1
+  digitalWrite(16,HIGH);//G1
+  delay(500);
+  }
